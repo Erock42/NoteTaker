ad5b3bad54eac750c7572025d225c58d065b01cf
Erock FaQue <eric.kemerer@gmail.com>
2022-03-13 23:03:25 -0400

    added api and html routes

--- server.js
+++ server.js
@@ -9,16 +9,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'../db/db.json'));
+});
 
-
-
-
-
-
-
-
+app.post("/api/notes",(req, res) => {
+function writeNote(body) {
+  const newNote = body;
+    fs.writeFileSync(__dirname, '../db/db.json'),
+    JSON.stringify({ newNote }, null)
+  return newNote;
+}});
 
 
 
